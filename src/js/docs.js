d5ce9d963e238542c32bf946c66b08b34a954513
Levi Thomason <me@levithomason.com>
2016-11-06 14:54:32 -0800

    begin anchors and doc site menu

--- src/js/docs.js
+++ src/js/docs.js
@@ -2,13 +2,13 @@
 // Docs
 // ----------------------------------------
 $(function() {
-  // insert code snippet after each content div for example
   var examples = document.querySelectorAll('.docs-example')
   var htmlBeautifyOptions = {
     indent_size: 2,
     unformatted: [], // format all elements
   }
 
+  // add code snippet
   examples.forEach(function(example) {
     var contents = example.querySelectorAll('.docs-example-content')
 
@@ -22,5 +22,21 @@ $(function() {
       $(content).after(pre)
       hljs.highlightBlock(pre)
     })
+  })
+
+  // add anchor links
+  anchors.add('.docs-example-title, .docs-example-header')
+
+  // generate menu from anchors
+  var docsMenu = document.querySelector('.docs-menu')
+  anchors.elements.forEach(function(anchor) {
+    console.log(anchor)
+    var menuItem = document.createElement('a')
+    if (anchor.className === 'docs-example-title') menuItem.className = 'item'
+    if (anchor.className === 'docs-example-header')  menuItem.className = 'sub item'
+    menuItem.href = '#' + anchor.id
+    menuItem.textContent = anchor.textContent
+
+    docsMenu.appendChild(menuItem)
   })
 })
