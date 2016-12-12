db584d9d09c34e8166ea2bf27b3f08af371231c1
James <jlassos@gmail.com>
2016-12-11 16:16:13 -0800

    feat/card (#25)
    * heading primary page and uppercase
    
    * headings scss
    
    * add secondary header
    
    * page header box
    
    * initial card
    
    * heading primary page and uppercase
    
    * headings scss
    
    * add secondary header
    
    * page header box
    
    * initial card
    
    * add some markup
    
    * scaffold out card
    
    * card styling
    
    * add helpers
    
    * standardize border-radius and box-shadow
    
    * add gray background and wider docs
    
    * rework rating, add basic card and divider
    
    * finish first draft card
    
    * fix pages
    
    * use unsplash for all images

--- src/js/docs.js
+++ src/js/docs.js
@@ -2,40 +2,40 @@
 // Docs
 // ----------------------------------------
 $(function() {
-  var examples = document.querySelectorAll('.docs-example')
+  var $examples = $('.docs.example')
   var htmlBeautifyOptions = {
     indent_size: 2,
     unformatted: [], // format all elements
   }
 
   // add code snippet
-  examples.forEach(function(example) {
-    var contents = example.querySelectorAll('.docs-example-content:not(.no-code)')
+  $examples.each(function() {
+    var $contents = $(this).children('.content')
 
-    contents.forEach(function(content) {
+    $contents.each(function() {
       var pre = document.createElement('pre')
       var code = document.createElement('code')
-      var html = content.innerHTML
+      var html = this.innerHTML
 
       code.textContent = html_beautify(html, htmlBeautifyOptions)
       pre.appendChild(code)
-      $(content).after(pre)
+      $(this).after(pre)
       hljs.highlightBlock(pre)
     })
   })
 
   // add anchor links
-  anchors.add('.docs-example-title, .docs-example-header')
+  anchors.add('.docs.example > .title, .docs.example > .header')
 
   // generate menu from anchors
-  var docsMenu = document.querySelector('.docs-menu')
+  var $docsMenu = $('.docs.menu')
   anchors.elements.forEach(function(anchor) {
     var menuItem = document.createElement('a')
-    if (anchor.className === 'docs-example-title') menuItem.className = 'item'
-    if (anchor.className === 'docs-example-header')  menuItem.className = 'sub item'
+    if (anchor.className === 'title') menuItem.className = 'item'
+    if (anchor.className === 'header') menuItem.className = 'sub item'
     menuItem.href = '#' + anchor.id
     menuItem.textContent = anchor.textContent
 
-    docsMenu.appendChild(menuItem)
+    $docsMenu.append(menuItem)
   })
 })
